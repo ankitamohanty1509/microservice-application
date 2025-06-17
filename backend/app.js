932b5c3e6c92219c96de84f778e6f66ccfc51d51
ankitamohanty1509 <148987768+ankitamohanty1509@users.noreply.github.com>
2025-06-17 09:43:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,23 @@ const port = 3000;
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: 'admin',
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'testdb'
+  database: process.env.DB_NAME
 });
 
-app.get('/', (req, res) => {
-  db.query('SELECT * FROM visitors', (err, result) => {
-    if (err) return res.send(err);
-    res.json(result);
+db.connect(err => {
+  if (err) {
+    console.error('DB Connection Error:', err.stack);
+    return;
+  }
+  console.log('Connected to DB');
+});
+
+app.get('/api', (req, res) => {
+  db.query('SELECT NOW() AS time', (err, results) => {
+    if (err) res.status(500).send(err.toString());
+    else res.send('Database time: ' + results[0].time);
   });
 });
 
